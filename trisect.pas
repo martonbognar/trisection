bf1d95a11403690b08e66b4c3acc5a1d6452e8d7
bognar marton <bognarmarci@gmail.com>
2013-12-15 04:36:23 +0100

    Improve code readability and add some comments

--- trisect.pas
+++ trisect.pas
@@ -1,74 +1,90 @@
 program trisection;
-uses crt, dos, graph;
+uses crt, graph;
 var
-i,sz,n                 : longint;
-x                      : real;
-gd,gm,angle,t,cx,cy,oc : integer;
-s,ts,as                : string;
+i, sz, n            : longint;  // i: loop variable, sz: numerator in the fraction, n: denominator in the fraction
+x                   : real;     // x: the fraction that gets multiplied with the original angle
+gd, gm              : integer;  // gd, gm: variables necessary for the graphical window
+angle, t, cx, oc    : integer;  // angle: the original angle, t: the calculated angle, cx: the width of the drawn pieslice, oc: the number of operations
+s, ts, as           : string;   // s: used to display the current angle, ts: used to display the current step, as: used to display the current inaccuracy
 
 begin
 
-sz := 1;
-n := 2;
-
 cx := 500;
-cy := 500;
 
-initgraph(gd,gm,'');
-detectgraph(gd,gm);
+initgraph(gd, gm, '');
+detectgraph(gd, gm);
 
 write('Enter an angle in degrees: ');
 readln(angle);
 write('Enter the number of operations: ');
 readln(oc);
 
-writeln('Now look at the graph window application');
+writeln('Now look at the graph window application!');
 
 randomize;
 setcolor(Random(15)+1);
 
-PieSlice(600,600,angle,0,cx);
+// draw the original angle
+PieSlice(600, 600, angle, 0, cx);
 
-str(angle,s);
+str(angle, s);
 s := s + #248;
-OutTextXY(1200,180,'Original:');
-OutTextXY(1300,180,s);
-OutTextXY(1300,200,'angle');
-OutTextXY(1350,200,'| inaccuracy');
+
+// print the size of the original angle and the headers
+OutTextXY(1200, 180, 'Original:');
+OutTextXY(1300, 180, s);
+OutTextXY(1300, 200, 'angle');
+OutTextXY(1350, 200, '| inaccuracy');
+
+sz := 1;
+n := 2;
 
 for i := 1 to oc do begin
+
+    // calculate the current angle size and inaccuracy
     x := sz/n;
     t := round(x*angle);
     x := (1/3)*angle - x*angle;
-    str(t,s);
-    str(i,ts);
-    str(x:9:9,as);
+
+    str(t, s);
+    str(i, ts);
+    str(x:9:9, as);
+
     ts := 'Step ' + ts + ':';
     s := s + #248;
     as := '| ' + as;
+
+    // print data about the current angle
     setcolor(Random(15)+1);
-    OutTextXY(1200,200+20*i,ts);
-    OutTextXY(1300,200+20*i,s);
-    OutTextXY(1350,200+20*i,as);
+    OutTextXY(1200, 200+20*i, ts);
+    OutTextXY(1300, 200+20*i, s);
+    OutTextXY(1350, 200+20*i, as);
 
     cx := cx - 10;
 
-    Pieslice(600,600,t,0,cx);
+    // draw the current angle
+    Pieslice(600, 600, t, 0, cx);
 
     if (n < 1073741823) then begin
+        // this...
         sz := sz * 2;
         n := n * 2;
     end else begin
-    OutTextXY(1200,200+20*i+20,'Memory limit reached, aborting.');
-    break;
+        OutTextXY(1200, 200+20*i+20, 'Memory limit reached, aborting.');
+        break;
     end;
 
+    // ...and this is where the magic happens
+    // we are using the following series to get as close to 1/3 as possible: 1/2, 1/4, 3/8, 5/16, 11/32, 21/64, ...
+    // (we can do this using only the allowed tools)
     if (i mod 2 = 0) then begin
         sz := sz + 1;
     end else begin
         sz := sz - 1;
     end;
+
     delay(500);
+
 end;
 
 writeln('Demonstration complete, press any key to exit.');
