b7bbbe9afc1ac782ddde141a23084f0d7fee71d9
bognar marton <bognarmarci@gmail.com>
2013-12-15 04:05:28 +0100

    Improve intelligibility and add a readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# What is this?
+
+This small project was originally created for a maths lesson to demonstrate trisectioning an angle using only compass-and-straightedge constructions.
+You can read more about the problem at [Wikipedia](http://en.wikipedia.org/wiki/Angle_trisection).
+The original code was written by Nohcra and myself.
+
+# Installation
+
+1. Install a Pascal compiler (for example [Free Pascal](http://www.freepascal.org/)).
+2. Compile `trisect.pas`
+3. Run the created .exe file
--- trisect.pas
+++ trisect.pas
@@ -1,10 +1,10 @@
-program asdf;
+program trisection;
 uses crt, dos, graph;
-var i,sz,n: longint;
-x: real;
-gd,gm,szog,t,cx,cy: integer;
-s: string;
-c: char;
+var
+i,sz,n                 : longint;
+x                      : real;
+gd,gm,angle,t,cx,cy,oc : integer;
+s,ts,as                : string;
 
 begin
 
@@ -17,45 +17,62 @@ cy := 500;
 initgraph(gd,gm,'');
 detectgraph(gd,gm);
 
-readln(szog);
+write('Enter an angle in degrees: ');
+readln(angle);
+write('Enter the number of operations: ');
+readln(oc);
+
+writeln('Now look at the graph window application');
+
 randomize;
 setcolor(Random(15)+1);
-PieSlice(600,500,szog,0,cx);
-//s := ('Az eredeti szog ', szog, #248, '. Ennek a harmada kb ', szog/3);
-OutTextXY(100,200,s);
 
-i := 1;
+PieSlice(600,600,angle,0,cx);
 
-repeat
-	setcolor(black);
-	OutTextXY(1500,200,s);
-	x := sz/n;
-	write(x:9:9, ' | ');
-	t := round(x*szog);
-	write(t, ' | ');
-	str(t,s);
-	s := s + #248;
-	setcolor(Random(15)+1);
-	OutTextXY(1500,200,s);
-	writeln();
-	
-	cx := cx - 10;
-	
-	Pieslice(600,500,t,0,cx);
-	
-	sz := sz * 2;
-	n := n * 2;
-	
-	if (i mod 2 = 0) then begin
-		sz := sz + 1;
-	end
-	else begin
-		sz := sz - 1;
-	end;
-	i := i + 1;
-	c := readkey;
-until (c = #27);
+str(angle,s);
+s := s + #248;
+OutTextXY(1200,180,'Original:');
+OutTextXY(1300,180,s);
+OutTextXY(1300,200,'angle');
+OutTextXY(1350,200,'| inaccuracy');
 
-closegraph;
+for i := 1 to oc do begin
+    x := sz/n;
+    t := round(x*angle);
+    x := (1/3)*angle - x*angle;
+    str(t,s);
+    str(i,ts);
+    str(x:9:9,as);
+    ts := 'Step ' + ts + ':';
+    s := s + #248;
+    as := '| ' + as;
+    setcolor(Random(15)+1);
+    OutTextXY(1200,200+20*i,ts);
+    OutTextXY(1300,200+20*i,s);
+    OutTextXY(1350,200+20*i,as);
+
+    cx := cx - 10;
+
+    Pieslice(600,600,t,0,cx);
+
+    if (n < 1073741823) then begin
+        sz := sz * 2;
+        n := n * 2;
+    end else begin
+    OutTextXY(1200,200+20*i+20,'Memory limit reached, aborting.');
+    break;
+    end;
+
+    if (i mod 2 = 0) then begin
+        sz := sz + 1;
+    end else begin
+        sz := sz - 1;
+    end;
+    delay(500);
+end;
+
+writeln('Demonstration complete, press any key to exit.');
 readkey;
+closegraph;
+
 end.
