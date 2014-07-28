baacf362cfd76d59b55456300a4d75c0ddc1ed56
bognar marton <bognarmarci@gmail.com>
2014-07-28 03:43:06 +0200

    Repeat the program and change when windows show up

--- trisect.pas
+++ trisect.pas
@@ -7,107 +7,112 @@ gd, gm              : integer;  // gd, gm: variables necessary for the graphical
 io                  : byte;     // io: used for input checking
 angle, t, cx, oc    : integer;  // angle: the original angle, t: the calculated angle, cx: the width of the drawn pieslice, oc: the number of operations
 s, ts, as           : string;   // s: used to display the current angle, ts: used to display the current step, as: used to display the current inaccuracy
+c                   : char;     // c: used to store the key pressed at the end
 
 begin
 
-cx := 500;
-
-initgraph(gd, gm, '');
-detectgraph(gd, gm);
-
-write('Enter an angle in degrees: ');
 repeat
-   {$I-}
-   readln(angle);
-   {$I+}
-   io:=ioresult;
-   if io <> 0 then writeln('Please enter a valid number');
-   if (angle < 1) and (io = 0) then writeln('Please only enter positive values');
-until (io = 0) and (angle > 0);
 
-write('Enter the number of operations: ');
-repeat
-   {$I-}
-   readln(oc);
-   {$I+}
-   io:=ioresult;
-   if io <> 0 then writeln('Please enter a valid number');
-   if (oc < 1) and (io = 0)  then writeln('Please only enter positive values');
-until (io = 0) and (oc > 0);
+    cx := 500;
 
-writeln('Now look at the graph window application!');
+    write('Enter an angle in degrees: ');
+    repeat
+       {$I-}
+       readln(angle);
+       {$I+}
+       io:=ioresult;
+       if io <> 0 then writeln('Please enter a valid number');
+       if (angle < 1) and (io = 0) then writeln('Please only enter positive values');
+    until (io = 0) and (angle > 0);
 
-randomize;
-setcolor(Random(15)+1);
+    write('Enter the number of operations: ');
+    repeat
+       {$I-}
+       readln(oc);
+       {$I+}
+       io:=ioresult;
+       if io <> 0 then writeln('Please enter a valid number');
+       if (oc < 1) and (io = 0)  then writeln('Please only enter positive values');
+    until (io = 0) and (oc > 0);
 
-// draw the original angle
-PieSlice(600, 600, angle, 0, cx);
+    writeln('Now look at the graph window application!');
 
-str(angle, s);
-s := s + #248;
+    initgraph(gd, gm, '');
+    detectgraph(gd, gm);
 
-// print the size of the original angle and the headers
-OutTextXY(1200, 180, 'Original:');
-OutTextXY(1300, 180, s);
-OutTextXY(1300, 200, 'angle');
-OutTextXY(1350, 200, '| inaccuracy');
-
-sz := 1;
-n := 2;
-
-for i := 1 to oc do begin
-
-    // calculate the current angle size and inaccuracy
-    x := sz/n;
-    t := round(x*angle);
-    x := (1/3)*angle - x*angle;
-
-    str(t, s);
-    str(i, ts);
-    str(x:9:9, as);
-    if (x > 0) then as := '+' + as;
-
-    ts := 'Step ' + ts + ':';
-    s := s + #248;
-    as := '| ' + as + #248;
-
-    // print data about the current angle
+    randomize;
     setcolor(Random(15)+1);
-    OutTextXY(1200, 200+20*i, ts);
-    OutTextXY(1300, 200+20*i, s);
-    OutTextXY(1350, 200+20*i, as);
 
-    cx := cx - 10;
+    // draw the original angle
+    PieSlice(600, 600, angle, 0, cx);
 
-    // draw the current angle
-    Pieslice(600, 600, t, 0, cx);
+    str(angle, s);
+    s := s + #248;
 
-    if (n < 1073741823) then begin
-        // this...
-        sz := sz * 2;
-        n := n * 2;
-    end else begin
-        OutTextXY(1200, 200+20*i+20, 'Memory limit reached, aborting.');
-        break;
+    // print the size of the original angle and the headers
+    OutTextXY(1200, 180, 'Original:');
+    OutTextXY(1300, 180, s);
+    OutTextXY(1300, 200, 'angle');
+    OutTextXY(1350, 200, '| inaccuracy');
+
+    sz := 1;
+    n := 2;
+
+    for i := 1 to oc do begin
+
+        // calculate the current angle size and inaccuracy
+        x := sz/n;
+        t := round(x*angle);
+        x := (1/3)*angle - x*angle;
+
+        str(t, s);
+        str(i, ts);
+        str(x:9:9, as);
+        if (x > 0) then as := '+' + as;
+
+        ts := 'Step ' + ts + ':';
+        s := s + #248;
+        as := '| ' + as + #248;
+
+        // print data about the current angle
+        setcolor(Random(15)+1);
+        OutTextXY(1200, 200+20*i, ts);
+        OutTextXY(1300, 200+20*i, s);
+        OutTextXY(1350, 200+20*i, as);
+
+        cx := cx - 10;
+
+        // draw the current angle
+        Pieslice(600, 600, t, 0, cx);
+
+        if (n < 1073741823) then begin
+            // this...
+            sz := sz * 2;
+            n := n * 2;
+        end else begin
+            OutTextXY(1200, 200+20*i+20, 'Memory limit reached, aborting.');
+            break;
+        end;
+
+        // ...and this is where the magic happens
+        // we are using the following series to get as close to 1/3 as possible: 1/2, 1/4, 3/8, 5/16, 11/32, 21/64, ...
+        // (we can do this using only the allowed tools)
+        if (i mod 2 = 0) then begin
+            sz := sz + 1;
+        end else begin
+            sz := sz - 1;
+        end;
+
+        delay(500);
+
+        if i = oc then OutTextXY(1200, 200+20*i+20, 'This was the last step.');
+
     end;
 
-    // ...and this is where the magic happens
-    // we are using the following series to get as close to 1/3 as possible: 1/2, 1/4, 3/8, 5/16, 11/32, 21/64, ...
-    // (we can do this using only the allowed tools)
-    if (i mod 2 = 0) then begin
-        sz := sz + 1;
-    end else begin
-        sz := sz - 1;
-    end;
+    OutTextXY(1200, 200+20*i+40, 'Demonstration complete, press any key to restart, ESC to quit.');
+    c := readkey;
+    closegraph;
 
-    delay(500);
-    
-    if i = oc then OutTextXY(1200, 200+20*i+20, 'This was the last step.');
-
-end;
-
-writeln('Demonstration complete, press any key to exit.');
-readkey;
-closegraph;
+until c = #27;
 
 end.
